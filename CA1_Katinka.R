d5944401925c177092984f67baeafdfedcd9cb6d
KatinkaWilkinson <katinka.wilkinson@gmail.com>
2025-02-20 11:34:39 +0200

    added Katinka's work

---
+++ CA1_Katinka.R
@@ -0,0 +1,39 @@
+CA1 <- read_csv("~/UCT 2025 Honours/Multivariate Analysis/Continuous Assessments/CA1/CA1.csv")
+View(CA1)
+
+dat <- as.matrix(CA1)
+
+# Question 1
+means <- matrix(nrow = 5, ncol = 5)
+for (i in 1:5) {
+  # calculate the means for all column for one time period
+  means[i,] = t(apply(dat[dat[,5] == i,], 2, mean))
+}
+
+means[,1:4]
+
+# can also use dplyr
+
+# alternative method: matrices -
+
+all_means <- matrix(nrow = 5, ncol = 5)
+for (i in 1:5) { # one loop for each time period
+  mat_i <- dat[dat[,5] == i,]
+  vec1 <- matrix(1, nrow = nrow(mat_i))
+  means_i <- (1/nrow(mat_i)) * vec1 %*% t(vec1) %*% mat_i
+  all_means[i,] <- means_i[1,]
+}
+
+print(all_means)
+
+# Question 2
+install.packages("corrplot")
+library(corrplot)
+
+for (i in 1:5) {
+  png(paste0("correlation_plot_period_", i, ".png"), width = 800, height = 600)
+  mat_i <- dat[dat[,5] == i,1:4]
+  varcovmat <- cor(mat_i)
+  corrplot(varcovmat, method = 'circle')
+  dev.off()
+}
