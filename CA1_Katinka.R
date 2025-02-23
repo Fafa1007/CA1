cdf15c498207126ba8a78fe756a418418d914ce8
KatinkaWilkinson <katinka.wilkinson@gmail.com>
2025-02-23 21:33:31 +0200

    Katinka Attempted Q3 and Q4

--- CA1_Katinka.R
+++ CA1_Katinka.R
@@ -37,3 +37,39 @@ for (i in 1:5) {
   corrplot(varcovmat, method = 'circle')
   dev.off()
 }
+
+# Question 3
+tp1 <- dat[dat[,5]==1,]
+X1 <- tp1[,1]
+X2 <- tp1[,3]
+vec1 <- matrix(1, nrow = length(X1))
+
+# method 1: use the deviance vectors
+
+dev_vec_X1 <- X1 - 1/length(X1) * vec1 %*% t(vec1) %*% X1
+dev_vec_X2 <- X2 - 1/length(X2) * vec1 %*% t(vec1) %*% X2
+angle <- acos((t(dev_vec_X1)%*% dev_vec_X2)/(norm(dev_vec_X1)*norm(dev_vec_X2)))
+angle # in radians
+
+# method 2: use the variance covariance matrix
+
+cov_mat <- cov(tp1)
+angle <- acos(cov_mat[1,3] / (sqrt(cov_mat[1,1])*sqrt(cov_mat[3,3])))
+angle # in radians
+
+# The angle is 1.5 radians, which is almost pi/2 (a 90 degree angle). This implies that the vectors are almost orthogonal, meaning that they have nearly no correlation
+
+# Question 4
+b <- matrix(c(-1, 0, 0, 3, 0), nrow = 5)
+y_means <- t(b) %*% t(all_means)
+y_means
+
+for (i in 1:5) {
+  mat_i <- dat[dat[,5] == i,]
+  cov_i <- cov(mat_i)
+  cov_y <- t(b) %*% cov_i %*% b
+  print(cov_y)
+}
+
+y_cov <- t(b) %*% cov(all_means) %*% b
+y_cov
